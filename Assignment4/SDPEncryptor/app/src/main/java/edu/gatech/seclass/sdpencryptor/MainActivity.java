c69f81173a8b511e41d8ffdac3aab75374d73ce8
SeGe Jung <sjung332@gatech.edu>
2022-06-06 19:28:52 -0700

    error messages

--- Assignment4/SDPEncryptor/app/src/main/java/edu/gatech/seclass/sdpencryptor/MainActivity.java
+++ Assignment4/SDPEncryptor/app/src/main/java/edu/gatech/seclass/sdpencryptor/MainActivity.java
@@ -2,13 +2,67 @@ package edu.gatech.seclass.sdpencryptor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button encryptButtonID;
+    private EditText entryTextID;
+    private EditText argInput1ID;
+    private EditText argInput2ID;
+    private TextView textEncryptedID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        encryptButtonID = (Button) findViewById(R.id.encryptButtonID);
+        entryTextID = (EditText) findViewById(R.id.entryTextID);
+        argInput1ID = (EditText) findViewById(R.id.argInput1ID);
+        argInput2ID = (EditText) findViewById(R.id.argInput2ID);
+        textEncryptedID = (TextView) findViewById(R.id.textEncryptedID);
     }
+
+    public void handleClick(View view) {
+        //handles error messages
+        String value = entryTextID.getText().toString();
+        String regex = "^.*[a-zA-Z0-9]+.*$";
+        boolean result = value.matches(regex);
+        if (value.length() == 0) {
+            Context context = getApplicationContext();
+            CharSequence text = "Error: Empty value";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            entryTextID.setError("Invalid Entry Text");
+            argInput1ID.setError("Invalid Arg Input 1");
+            argInput2ID.setError("Invalid Arg Input 2");
+        }
+        if (result) {
+        }
+        //handles that there must be at least one letter or number
+        else {
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_LONG;
+            CharSequence text2 = "Error: Must contain at least one letter or number";
+            Toast toast2 = Toast.makeText(context, text2, duration);
+            toast2.show();
+            entryTextID.setError("Invalid Entry Text");
+        }
+
+
+
+            //textEncryptedID.setText(result);
+    }
+
+
+
 }
