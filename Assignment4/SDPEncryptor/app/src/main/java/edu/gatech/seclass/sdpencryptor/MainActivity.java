44ca744d6cf257a0a651f8564efb4937ba2b503b
SeGe Jung <sjung332@gatech.edu>
2022-06-08 19:07:14 -0700

    textview showing calculation

--- Assignment4/SDPEncryptor/app/src/main/java/edu/gatech/seclass/sdpencryptor/MainActivity.java
+++ Assignment4/SDPEncryptor/app/src/main/java/edu/gatech/seclass/sdpencryptor/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText argInput1ID;
     private EditText argInput2ID;
     private TextView textEncryptedID;
+    private String answer;
 
 
     @Override
@@ -30,7 +31,17 @@ public class MainActivity extends AppCompatActivity {
         argInput1ID = (EditText) findViewById(R.id.argInput1ID);
         argInput2ID = (EditText) findViewById(R.id.argInput2ID);
         textEncryptedID = (TextView) findViewById(R.id.textEncryptedID);
+
+
     }
+
+    private String encrypt(String Data, String arg1, String arg2) {
+        int finalvalue = Integer.parseInt(arg1);
+        int finalvalue2 = Integer.parseInt(arg2);
+        answer = String.valueOf((finalvalue * 4 + finalvalue2) % 36);
+        return answer;
+    }
+
     static boolean isPrime(int n)
     {
         if (n < 36 && n == 1 || n == 5 || n ==7 || n == 11 || n == 13 || n == 17 || n == 19 || n == 23 || n == 25 || n == 29 || n == 31 || n == 35) {
@@ -41,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     public void handleClick(View view) {
         //handles error messages
         String answer = "a";
@@ -49,10 +59,9 @@ public class MainActivity extends AppCompatActivity {
         String value2 = argInput1ID.getText().toString();
         String value3 = argInput2ID.getText().toString();
         String regex = "^.*[a-zA-Z0-9]+.*$";
-        
-        textEncryptedID.setText(answer);
 
         boolean result = value.matches(regex);
+
 
         if (isPrime(Integer.valueOf(value2)) == false) {
 
@@ -64,11 +73,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (value.length() == 0) {
-            Context context = getApplicationContext();
-            CharSequence text = "Error: Empty value";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(context, text, duration);
-            //toast.show();
             entryTextID.setError("Invalid Entry Text");
         }
         if (value2.length() == 0) {
@@ -77,22 +81,18 @@ public class MainActivity extends AppCompatActivity {
         if (value3.length() == 0) {
             argInput2ID.setError("Invalid Arg Input 2");
         }
-        
+
         if (result) {
         }
         //handles that there must be at least one letter or number
         else {
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_LONG;
-            CharSequence text2 = "Error: Must contain at least one letter or number";
-            Toast toast2 = Toast.makeText(context, text2, duration);
-            //toast2.show();
             entryTextID.setError("Invalid Entry Text");
         }
-        
-      
+        if (result == true && value3.length() != 0 && value2.length() != 0 && value.length() != 0 && isPrime(Integer.valueOf(value3)) == true && isPrime(Integer.valueOf(value2)) == true) {
+            answer = encrypt(entryTextID.getText().toString(), argInput1ID.getText().toString(), argInput2ID.getText().toString());
+            textEncryptedID.setText(answer);
+        }
+
     }
-
-
 
 }
